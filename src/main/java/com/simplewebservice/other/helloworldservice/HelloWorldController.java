2ec5c358228163f58e92c41c3a74dd90db67e6af
akorchevskyi <akorchevskyi@talend.com>
2021-02-23 10:55:44 +0200

    implement internationalization

--- src/main/java/com/simplewebservice/other/helloworldservice/HelloWorldController.java
+++ src/main/java/com/simplewebservice/other/helloworldservice/HelloWorldController.java
@@ -1,11 +1,19 @@
 package com.simplewebservice.other.helloworldservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
+import java.util.Locale;
+
+@RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping("/hello-world")
     public String helloWorld() {
@@ -20,5 +28,10 @@ public class HelloWorldController {
     @GetMapping("/hello-world-bean/{name}")
     public HelloWorldBean helloWorldBeanPathVar(@PathVariable String name) {
         return new HelloWorldBean(String.format("HelloWorldBean, %s", name));
+    }
+
+    @GetMapping("/hello-world-int")
+    public String helloWorldInt(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+        return messageSource.getMessage("hello.world.message", null, locale);
     }
 }
